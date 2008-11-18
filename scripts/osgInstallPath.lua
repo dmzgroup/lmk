78f9314046d97653eb0ab6e132cdf698c0c944e6
Randall E. Barker <simstorm@mac.com>
2008-11-17 16:29:52 -0800

    adding script to adjust osg lib paths

---
+++ scripts/osgInstallPath.lua
@@ -0,0 +1,38 @@
+require "lmkbase"
+require "lmkutil"
+
+local OldPath = "/Users/barker/3rdparty/OpenSceneGraph-2.6.1/lib/Release/"
+local NewPath = "@executable_path/../Frameworks/osg/"
+local libs = {
+"libOpenThreads.dylib",
+"libosg.dylib",
+"libosgDB.dylib",
+"libosgFX.dylib",
+"libosgGA.dylib",
+"libosgManipulator.dylib",
+"libosgParticle.dylib",
+"libosgShadow.dylib",
+"libosgSim.dylib",
+"libosgTerrain.dylib",
+"libosgText.dylib",
+"libosgUtil.dylib",
+"libosgViewer.dylib",
+"libosgWidget.dylib",
+}
+
+local list = {}
+
+for _, lib in ipairs (libs) do
+   list[#list + 1] = {OldPath .. lib, NewPath .. lib}
+end
+
+local files = lmkbase.files (".")
+
+for index, file in ipairs (files) do
+   local id = "install_name_tool -id @executable_path/../Frameworks/osg/" .. file .. " " .. file
+   os.execute (id)
+   for _, value in ipairs (list) do
+      local arg = "install_name_tool -change " .. value[1] .. " " .. value[2] .. " " .. file
+      os.execute (arg)
+   end
+end
